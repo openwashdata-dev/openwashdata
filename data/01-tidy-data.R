ef2952b797721fea9b1b52a30a29feaee127da3f
Lars Schöbitz <lars@lse.de>
2022-04-12 10:50:17 +0200

    Add example script

---
+++ data/01-tidy-data.R
@@ -0,0 +1,10 @@
+# Description -------------------------------------------------------------
+
+# Example script that is used to clean data from raw_data directory
+# and store in tidy_data directory. 
+
+# Import data -------------------------------------------------------------
+
+# This script is a minimal exmaple which does not contain any code
+
+
