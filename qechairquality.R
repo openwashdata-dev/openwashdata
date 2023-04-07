4979e5c0aa64e1471f0e0ff49fbc1cb47cfbdf8d
Lars Schöbitz <lars@lse.de>
2023-04-07 11:41:42 +0200

    Delete

--- qechairquality.R
+++
@@ -1,6 +0,0 @@
-\describe{
-\item{` date_time`}{ Timestamp of the air quality measurement.}
-\item{` location`}{ Label for sensor location.}
-\item{` indicator`}{ Air quality measurement indicator (pm2.5 and pm10).}
-\item{` value`}{ Measured value in µg/m3.}
-}
