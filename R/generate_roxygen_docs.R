4c5d5ad663b7109d86b1c1365b9f23164e337107
Lars Schöbitz <lars@lse.de>
2023-04-07 12:08:05 +0200

    Complete documentation and check
    First working version of generate_roxygen_docs function. Completed documentation for the function.

--- R/generate_roxygen_docs.R
+++ R/generate_roxygen_docs.R
@@ -1,53 +1,54 @@
 #' Generate roxygen2 documentation from a CSV file
 #'
-#' This function takes a CSV table with columns `variable_name` and `description` as input 
+#' This function takes a CSV table with columns `variable_name` and `description` as input
 #' and outputs roxygen2 documentation for `\describe` and `\item`.
 #'
 #' @param input_file_path Path to the input CSV file.
 #' @param output_file_path Path to the output file that will contain the roxygen2 documentation.
 #'
 #' @return NULL
-#' 
-#' @examples
+#'
+#' @examples \dontrun{
 #' # Generate roxygen2 documentation from example.csv
 #' generate_roxygen_docs("example.csv", "output.txt")
+#' }
 #'
-#' @importFrom utils writeLines
-#' @importFrom base read.csv
+
+#'
 #' @export
 
 generate_roxygen_docs <- function(input_file_path, output_file_path) {
-  
+
   # read input CSV file
   input_df <- read.csv(input_file_path)
-  
+
   # initialize output character vector
   output <- character()
-  
+
   # create \describe block
   output <- c(output, paste0("#' ", "\\describe{"))
-  
+
   # iterate over input rows and create \item blocks
   for (i in seq_len(nrow(input_df))) {
     variable_name <- input_df[i, "variable_name"]
     description <- input_df[i, "description"]
-    
+
     # create \item block
     item <- paste0("#'   ", "\\item{", variable_name, "}{", description, "}")
-    
+
     # append to output
     output <- c(output, item)
   }
-  
+
   # close \describe block
   output <- c(output, "#' }")
-  
+
   # write output to file
   writeLines(output, output_file_path)
 }
 
 # Here's an explanation of how the function works:
-# 
+#
 # - The function takes two arguments: input_file_path, the path to the input CSV file, and output_file_path, the path to the output file that will contain the roxygen2 documentation.
 # - The input CSV file is read in using the read.csv() function and stored in the input_df data frame.
 # - An empty character vector is initialized to store the output roxygen2 documentation.
@@ -58,5 +59,5 @@ generate_roxygen_docs <- function(input_file_path, output_file_path) {
 # - The \item block is added to the output vector using the c() function.
 # - Once all rows have been processed, the \describe block is closed by adding the string "}" to the output vector using the c() function.
 # - The output roxygen2 documentation is written to a file at the specified output_file_path using the writeLines() function.
-# 
+#
 # Note that this function assumes that the input CSV file has columns named variable_name and description, and that the output roxygen2 documentation should consist of a single \describe block containing multiple \item blocks. You may need to modify the function to suit your specific needs.
--- R/write_cff.R
+++ R/write_cff.R
@@ -10,66 +10,66 @@
 
 # code --------------------------------------------------------------------
 
-library(cffr)
-
-# Download desc
-tempdesc <- "DESCRIPTION"
-
-# Default behaviour
-default <- cff_create(tempdesc, dependencies = FALSE)
-
-length(default$authors)
-
-default
-
-# Get all persons with given roles
-# desc_path: the path to the DESCRIPTION FILE
-# roles: vector with the roles to extract
-
-parse_desc_roles <- function(desc_path = ".", roles = c("aut", "cre")) {
-  pkg <- desc::desc(desc_path)
-  
-  # This extracts all the persons
-  persons <- as.person(pkg$get_authors())
-  
-  locate_roles <- vapply(persons, function(x, r = roles) {
-    any(x$role %in% roles)
-  }, logical(1))
-  
-  person_roles <- persons[locate_roles]
-  
-  parse_all_roles <- lapply(person_roles, cff_parse_person)
-  parse_all_roles <- unique(parse_all_roles)
-  
-  class(parse_all_roles) <- "cff"
-  parse_all_roles
-}
-
-# Get the persons with your desired roles
-newauth <- parse_desc_roles(
-  tempdesc,
-  roles = c("aut", "cre", "ctb", "fnd")
-)
-
-newauth
-
-# Modify the default object
-mod <- default
-
-mod$authors <- newauth
-
-length(mod$authors)
-
-newkeys <- list(
-  #"data.doi" = "https://doi.org/10.5281/zenodo.7708629",
-  "type" = "dataset"
-)
-
-modobject <- cff_create(mod, keys = newkeys)
-
-cff_validate(modobject)
-
-# Write the new object.
-
-cff_write(modobject)
+# library(cffr)
+#
+# # Download desc
+# tempdesc <- "DESCRIPTION"
+#
+# # Default behaviour
+# default <- cff_create(tempdesc, dependencies = FALSE)
+#
+# length(default$authors)
+#
+# default
+#
+# # Get all persons with given roles
+# # desc_path: the path to the DESCRIPTION FILE
+# # roles: vector with the roles to extract
+#
+# parse_desc_roles <- function(desc_path = ".", roles = c("aut", "cre")) {
+#   pkg <- desc::desc(desc_path)
+#
+#   # This extracts all the persons
+#   persons <- as.person(pkg$get_authors())
+#
+#   locate_roles <- vapply(persons, function(x, r = roles) {
+#     any(x$role %in% roles)
+#   }, logical(1))
+#
+#   person_roles <- persons[locate_roles]
+#
+#   parse_all_roles <- lapply(person_roles, cff_parse_person)
+#   parse_all_roles <- unique(parse_all_roles)
+#
+#   class(parse_all_roles) <- "cff"
+#   parse_all_roles
+# }
+#
+# # Get the persons with your desired roles
+# newauth <- parse_desc_roles(
+#   tempdesc,
+#   roles = c("aut", "cre", "ctb", "fnd")
+# )
+#
+# newauth
+#
+# # Modify the default object
+# mod <- default
+#
+# mod$authors <- newauth
+#
+# length(mod$authors)
+#
+# newkeys <- list(
+#   #"data.doi" = "https://doi.org/10.5281/zenodo.7708629",
+#   "type" = "dataset"
+# )
+#
+# modobject <- cff_create(mod, keys = newkeys)
+#
+# cff_validate(modobject)
+#
+# # Write the new object.
+#
+# cff_write(modobject)
 
