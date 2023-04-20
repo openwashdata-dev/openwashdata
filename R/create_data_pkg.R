004041c0263ec3f0262285c3eae56e75d73e9d12
Your Name <Your Email>
2023-04-21 01:11:15 +0200

    backing initial workflow to pkg ideas

---
+++ R/create_data_pkg.R
@@ -0,0 +1,74 @@
+
+#' Create a New openwashdata Dataset R Package.
+#'
+#' This function creates a new data R package with the specified name and author information.
+#' It automatically creates a git repository and remote git data repository within the openwashdata
+#' GitHub organization. Note that the functions requires a GitHub account that
+#' has write access to the openwashdata GitHub organization as well as up and running RSA
+#' key pair authentication with GitHub set up. Personal access tokens will be
+#' created in the process though.
+#'
+#'
+#' @param pkg_name The name of the package to create.
+#' @param user.name The name of the package author.
+#' @param user.email The email address of the package author.
+#' @param location The location where the package should be created.
+#' @param open A logical value indicating whether to open the package directory in a new window.
+#' @importFrom gert git_init git_config_global
+#' @importFrom usethis create_package proj_set proj_get create_github_token use_github use_git_config
+#' @importFrom gitcreds gitcreds_set
+#' @importFrom gh gh_whoami
+#' @export
+#' @examples
+#' create_data_pkg("my_package", "John Doe", "johndoe@example.com")
+create_data_pkg <- function(pkg_name,
+                            user.name = NULL,
+                            user.email = NULL,
+                            location = NULL,
+                            open = FALSE){
+
+  # TODO
+
+  # maybe store things to status environment
+  # walk through datasteps
+  # citation process separately.
+  # github push probably separate process
+
+
+  if(is.null(location)){
+    pkg_path <- file.path(dirname(getwd()), pkg_name)
+  } else {
+    pkg_path <- pkg_name
+  }
+
+
+  if(is.null(user.name)){
+    usr_tbl <- git_config_global()
+    user.name <- as.character(usr_tbl[1,2])
+  }
+
+  if(is.null(user.email)){
+    usr_tbl <- git_config_global()
+    user.email <- as.character(usr_tbl[2,2])
+  }
+
+  cwd <- getwd()
+  create_package(pkg_path, open = open)
+  proj_set(pkg_path)
+  use_git_config(
+    scope = "project",
+    user.name = user.name,
+    user.email = user.email
+  )
+  use_git()
+
+  tkn <- gh_whoami()
+  if(is.null(tkn)){
+    create_github_token()
+    gitcreds_set()
+  } else {
+    message("GitHub Personal Access Token (PAT) already exists and set.")
+  }
+  use_github("openwashdata", protocol = "ssh")
+  proj_set(cwd)
+}
--- inst/demo.R
+++ inst/demo.R
@@ -1,7 +1,21 @@
-library(usethis)
-
-
-create_data_pkg <- function(pkg_name, location = NULL,
+#' Create a new R package.
+#'
+#' This function creates a new R package with the specified name and author information.
+#'
+#' @param pkg_name The name of the package to create.
+#' @param user.name The name of the package author.
+#' @param user.email The email address of the package author.
+#' @param location The location where the package should be created.
+#' @param open A logical value indicating whether to open the package directory in a new window.
+#' @importFrom gert git_init
+#' @importFrom usethis create_package
+#' @export
+#' @examples
+#' create_data_pkg("my_package", "John Doe", "johndoe@example.com")
+create_data_pkg <- function(pkg_name,
+                            user.name = "Your Name",
+                            user.email = "Your Email",
+                            location = NULL,
                             open = FALSE){
   if(is.null(location)){
     pkg_path <- file.path(dirname(getwd()), pkg_name)
@@ -9,13 +23,23 @@ create_data_pkg <- function(pkg_name, location = NULL,
     pkg_path <- pkg_name
   }
 
+  use_git_config(
+    scope = "project",
+    user.name = user.name,
+    user.email = user.email
+  )
 
-  curr_dir <- getwd()
+
   create_package(pkg_path, open = open)
-  setwd(pkg_path)
-  git()
-  setwd(curr_dir)
+  git_init(pkg_path)
+  # curr_dir <- getwd()
+  # setwd(pkg_path)
+  # use_github("openwashdata",protocol = "ssh")
+  # setwd(curr_dir)
 }
 
 
+# issue is get_remotes()
+# because the current directory has a remote orign, it is being used.
+debug(use_github)
 create_data_pkg("moodata")
