73eaac8d5ca2ff4f975bc99b6ad1844ee40482b4
Your Name <Your Email>
2023-06-17 22:50:48 +0200

    init function added

--- R/init.R
+++ R/init.R
@@ -1,6 +1,10 @@
 #' Copy Workflow Template
 #'
+#' Copies workflow template file into the current working directory
+#' and opens the file if RStudio is the editor.
+#'
 #' @export
-init <- function(){
-
+init <- function(target = file.path(getwd(),"bootstrap_pkg.R")){
+  p <- system.file("workflow_template.R", package = "openwashdata")
+  file.copy(p, target)
 }
